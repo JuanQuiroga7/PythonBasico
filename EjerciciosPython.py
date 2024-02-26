ebba93ae1a751d7bacc37e3264d333306a446902
JuanQuiroga7 <160855455+JuanQuiroga7@users.noreply.github.com>
2024-02-26 13:46:46 -0500

    Encontrar numero primo

--- EjerciciosPython.py
+++ EjerciciosPython.py
@@ -1,12 +1,19 @@
-def factorial (numero):
-    if numero == 0:
-        return 1
-    else:
-        return numero * factorial(numero - 1)
+def numero_primo(numero):
+    if numero <= 1:
+        return False
+    for i in range(2, int(numero**0.5) + 1):
+        if numero % i == 0: 
+            return False
+        return True
 
-numero = int(input("Escribe el numero que usaremos: "))
-resultado = factorial (numero)
 
-print("El numero factorial de ", numero, "es ", resultado)
+numero = int(input("Ingrese el numero para verificar si es primo :D --> "))
+
+print()
+if numero_primo(numero):
+    print("El numero ", numero, "es primo!! ")
+else:
+    print("El numero ", numero , "no es primo")
+
 
 
