d8f60a60273b01ee9ad95bccc78cffbca1632f55
JuanQuiroga7 <160855455+JuanQuiroga7@users.noreply.github.com>
2024-02-26 14:09:00 -0500

    Conversor Celsius a Fahrenheit

--- EjerciciosPython.py
+++ EjerciciosPython.py
@@ -1,19 +1,10 @@
-def numero_primo(numero):
-    if numero <= 1:
-        return False
-    for i in range(2, int(numero**0.5) + 1):
-        if numero % i == 0: 
-            return False
-        return True
+num = float(input("Ingrese la temperatura a convertir: "))
 
-
-numero = int(input("Ingrese el numero para verificar si es primo :D --> "))
+conversion = (num * 1.8) + 32 
 
 print()
-if numero_primo(numero):
-    print("El numero ", numero, "es primo!! ")
-else:
-    print("El numero ", numero , "no es primo")
+print(num, "celsius ","son ",conversion,"Fahrenheit")
+
 
 
 
