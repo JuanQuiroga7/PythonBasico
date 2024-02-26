96b8ccd26a0014db30e0dc620b751c6d904e24be
JuanQuiroga7 <160855455+JuanQuiroga7@users.noreply.github.com>
2024-02-26 12:52:43 -0500

    Mayor de tres numeros

--- EjerciciosPython.py
+++ EjerciciosPython.py
@@ -1,6 +1,16 @@
-num1 = input("Escriba el primer numero: ")
-num2 = input("Escriba el segundo numero: ")
-resultado = int(num1) + int(num2)
-print(resultado)
+def numero_mayor(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+num1 = int(input("Profe, ingrese el primer numero: "))
+num2 = int(input("Profe, ingrese el segundo numero: "))
+num3 = int(input("Profe, ingrese el tercer numero: "))
+
+resultado = numero_mayor(num1, num2, num3)
+print("El numero mayor es ", resultado)
 
 
