e284d15b711899b24118198f8625bb1adec61a29
JuanQuiroga7 <160855455+JuanQuiroga7@users.noreply.github.com>
2024-02-26 23:34:18 -0500

    Hallar numero promedio

--- EjerciciosPython.py
+++ EjerciciosPython.py
@@ -1,8 +1,10 @@
 lista = [23, 34, 33, 15, 34, 34, 21, 12, 12, 86, 12, 43, 34, 54, 24, 55, 12, 42, 78]
 
-promedio = sum(lista) / len(lista)
+promedio = int(sum(lista) / len(lista))
 
-print("El promedio de los numeros de la lista es: ", promedio)
+print("Edad de todos los estudiantes: ", lista)
+print()
+print("La edad promedio de los estudiantes es : ",promedio, "años")
 
 
 
